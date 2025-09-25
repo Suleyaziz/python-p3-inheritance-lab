931a034e421ec49ec58b3612b7e6727331b91c76
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-09-25 09:21:45 +0300

    Lab complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,15 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "The Earth revolves around the Sun.",
+            "Water boils at 100 degrees Celsius.",
+            "The capital of France is Paris.",
+            "Python is a programming language.",
+            "The Great Wall of China is visible from space."
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
